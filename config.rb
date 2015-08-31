f08e2a630a1615816645c15cba7df88e1e24c0e0
Hafiz Khairudin <ibnukamy@gmail.com>
2015-08-31 21:07:46 +0800

    add bootstrap

--- config.rb
+++ config.rb
@@ -1,77 +1,42 @@
-###
-# Compass
-###
+require "extensions/views"
+require "slim"
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+activate :views
+activate :directory_indexes
 
-###
-# Page options, layouts, aliases and proxies
-###
+set :relative_links, true
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :layout, 'layouts/application'
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+ activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
 end
 
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
+
+helpers do
+  def nav_link(link_text, page_url, options = {})
+    options[:class] ||= ""
+    if current_page.url.length > 1
+      current_url = current_page.url.chop
+    else
+      current_url = current_page.url
+    end
+    options[:class] << " active" if page_url == current_url
+    link_to(link_text, page_url, options)
+  end
+end
 after_configuration do
   if defined?(RailsAssets)
     RailsAssets.load_paths.each do |path|
